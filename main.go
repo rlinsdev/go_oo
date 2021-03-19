ddc79fa76f5a789995a4427e9c4632adb9e9cfd3
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-19 20:15:14 -0300

    Structure

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	titular := "Murldock"
-	agencia := 589
-	numeroConta := 123456
-	saldo := 150.56
+type ContaCorrente struct {
+	titular string
+	numeroAgencia int
+	numeroConta int
+	saldo float64
+}
 
-	fmt.Println(titular, agencia, numeroConta, saldo)
+func main() {
+
+	contaMurldock := ContaCorrente {
+		"Murldock",
+		589,
+		12,
+		15.56,
+	}
+
+	
+
+	
+
+	fmt.Println(contaMurldock)
 
 }
