3cf7d201fa4b073b684443f8fc2eddb373403015
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-19 19:22:00 -0300

    Create main class

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Murldock"
+	agencia := 589
+	numeroConta := 123456
+	saldo := 150.56
+
+	fmt.Println(titular, agencia, numeroConta, saldo)
+
+}
