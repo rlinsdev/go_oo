29551ee6a29181b8990ce2ed35236746b35a8995
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-21 10:13:59 -0300

    refactoring

--- main.go
+++ main.go
@@ -1,54 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
-}
+	c "github.com/banco/contas"
+)
 
-func (c * ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 &&  valorDoSaque<= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"		
-	}
-}
-
-func (c * ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito >0 {
-		c.saldo+=valorDoDeposito
-		return "Depósito relizado com sucesso", c.saldo
-	} else {
-		return "Depósito inválido", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) (bool) {
-	if valorTransferencia < c.saldo && valorTransferencia > 0 {
-		c.saldo-=valorTransferencia
-		status, saldo := contaDestino.Depositar(valorTransferencia)
-		fmt.Println(status, saldo)
-		return true
-	} else {
-		return false
-	}
-}
 
 func main() {
 
-	contaDaSilva := ContaCorrente {titular: "Silvia", saldo: 300}
-	contaDoGustavo := ContaCorrente {titular: "Gustavo", saldo: 100}
+	contaDaSilva := c.ContaCorrente {Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := c.ContaCorrente {Titular: "Gustavo", Saldo: 100}
 
 	status := contaDaSilva.Transferir(200, &contaDoGustavo)
 	fmt.Println(status)
 
-	fmt.Println(contaDaSilva.saldo)
-	fmt.Println(contaDoGustavo.saldo)
+	fmt.Println(contaDaSilva.Saldo)
+	fmt.Println(contaDoGustavo.Saldo)
 
 
 }
